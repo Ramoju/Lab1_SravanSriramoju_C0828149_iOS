9293b3731685c9d48f1c44ac734685b980358a14
Sravan8232 <C0828149@mylambton.ca>
2022-01-18 10:52:18 -0500

    Initial Commit

---
+++ Lab1_SravanSriramoju_C0828149_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_SravanSriramoju_C0828149_iOS
+//
+//  Created by Sravan Sriramoju on 2022-01-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
